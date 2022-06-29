29ea764299bfe2a11f74ba712fd01bfb1ad5186f
Hhhxdjejjd <99557090+Hhhxdjejjd@users.noreply.github.com>
2022-06-29 22:39:36 +0000

    jrmdk

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1302,6 +1302,9 @@ class PlayState extends MusicBeatState
 					if(daSong == 'roses') FlxG.sound.play(Paths.sound('ANGRY'));
 					schoolIntro(doof);
 
+				case 'how to funk':
+				     startDialogue(dialogueJson)
+
 				case 'ugh' | 'guns' | 'stress':
 					tankIntro();
 
